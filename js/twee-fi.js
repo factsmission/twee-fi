c2de210efad7d8347e586d2d57917cb7e7139fb5
Bob Walsh <bob.walsh@47hats.com>
2017-09-22 16:03:36 -0700

    Regex check of tweet url and appropriate text message.

--- js/twee-fi.js
+++ js/twee-fi.js
@@ -1,5 +1,18 @@
 $(function(){
-  $('input').on('change', function(){
-    console.log("Changed: " + $(this).val());
+  $('input').on('change keyup paste', function(){
+    $("#tweet-valid").removeClass("show").addClass("hide");
+    $("#tweet-invalid").removeClass("show").addClass("hide");
+  });
+
+  $('#check_tweet').on('click',function(){
+    // Regex-pattern to check URLs against. 
+    // https://twitter.com/<twitteruser>/status/<long number>
+    var urlRegex = /^https:\/\/twitter.com\/[a-zA-Z]+\/status\/[0-9]*$/;
+    var tweet_url = $('#tweet_url').val();
+    if (urlRegex.test(tweet_url)) {
+      $("#tweet-valid").removeClass("hide").addClass("show");
+    } else {
+      $("#tweet-invalid").removeClass("hide").addClass("show");
+    }
   });
 });
