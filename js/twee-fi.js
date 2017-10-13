f3f757fd3701e1f5bab8eb7aeae46095a7f93fc4
Reto Gmür <me@farewellutopia.com>
2017-10-13 00:03:59 +0000

    incomplete tidying

--- js/twee-fi.js
+++ js/twee-fi.js
@@ -64,17 +64,6 @@ $(function () {
 
     $('#default_timestamp').html(default_timestamp());
     $('[data-toggle="tooltip"]').tooltip();
-    $('#tweet_url').on('change keyup paste', function () {
-        $("#tweet-valid").removeClass("show").addClass("hide");
-        $("#tweet-invalid").removeClass("show").addClass("hide");
-        checkData();
-    });
-    $('#claim_reviewed, #ldp-uri, #review_body').on('change keyup paste', function () {
-        checkData();
-    });
-    $('#rating-in-stars').on('change', function () {
-        checkData();
-    });
 
 
     class TweetUri {
@@ -83,6 +72,7 @@ $(function () {
             // https://twitter.com/<twitteruser>/status/<long number>
             var urlRegex = /^https:\/\/twitter.com\/([a-zA-Z _.,!"'/$]+)\/status\/([0-9]*$)/;
             this.match = urlRegex.exec(value);
+            this.value = value;
         }
 
         isValid() {
@@ -95,6 +85,10 @@ $(function () {
 
         getStatus() {
             return this.match[2];
+        }
+        
+        toString() {
+            return this.value;
         }
     }
 
@@ -114,178 +108,123 @@ $(function () {
         $("#invalid_url").removeClass("visible").addClass("invisible");
     });
 
-    function checkData() {
-        const LDP_neg_msg = "Missing LDP-URI.";
-        const Tweet_url_neg_msg = "Missing Valid Tweet URL.";
-        const Claim_neg_msg = "Missing Claim made in Tweet.";
-        const Review_body_neg_msg = "Missing the body of your Review.";
-        const Rating_neg_msg = "Missing Rating.";
-        const LDP_msg = "LDP-URI appears valid";
-        const Tweet_url_msg = "Tweet URL appears valid";
-        const Claim_msg = "Claim in Tweet described.";
-        const Rating_msg = "Claim Rated.";
-        const Review_body_msg = "Review of Tweet described.";
-        if ($('#ldp-uri').val() == false) {
-            $('#valcheck_1_msg').text(LDP_neg_msg);
-            $('#valcheck_1_ck').prop('checked', false);
-        } else {
-            $('#valcheck_1_msg').text(LDP_msg);
-            $('#valcheck_1_ck').prop('checked', true);
-        }
-        if (!(new TweetUri($('#tweet_url').val())).isValid()) {
-            $('#valcheck_2_msg').text(Tweet_url_neg_msg);
-            $('#valcheck_2_ck').prop('checked', false);
-        } else {
-            $('#valcheck_2_msg').text(Tweet_url_msg);
-            $('#valcheck_2_ck').prop('checked', true);
-        }
-        if ($('#claim_reviewed').val() == false) {
-            $('#valcheck_3_msg').text(Review_body_neg_msg);
-            $('#valcheck_3_ck').prop('checked', false);
+    //Only after the user tried to submit the input is validated
+    var submissionAttempted = false;
+    
+    $('#claim_reviewed,#review_body').on('change keyup paste', function () {
+        if (submissionAttempted) checkForm();
+    });
+    $('#rating-in-stars').on('change', function () {
+        if (submissionAttempted) checkForm();
+    });
+   
+    var form = $("#review-form");
+    function checkForm() {
+        var formIsValid = form[0].checkValidity();
+        //the first two shuld actually be talen care of by bootstrap
+        if (!$('#claim_reviewed').val()) {
             $("#no_claim").removeClass("invisible").addClass("visible");
         } else {
-            $('#valcheck_3_msg').text(Review_body_msg);
-            $('#valcheck_3_ck').prop('checked', true);
             $("#no_claim").removeClass("visible").addClass("invisible");
         }
-        if ($('#review_body').val() == false) {
-            $('#valcheck_4_msg').text(Claim_neg_msg);
-            $('#valcheck_4_ck').prop('checked', false);
+        if (!$('#review_body').val()) {
             $("#no_body").removeClass("invisible").addClass("visible");
         } else {
-            $('#valcheck_4_msg').text(Claim_msg);
-            $('#valcheck_4_ck').prop('checked', true);
             $("#no_body").removeClass("visible").addClass("invisible");
         }
-        if ($('#rating-in-stars').val() == 0) {
-            $('#valcheck_5_msg').text(Rating_neg_msg);
-            $('#valcheck_5_ck').prop('checked', false);
-            $("#no_rating").removeClass("invisible").addClass("visible");
+        if (Number($('#rating-in-stars').val()) === 0) {
+            formIsValid = false;
+            $("#no_rating").show()
         } else {
-            $('#valcheck_5_msg').text(Rating_msg);
-            $('#valcheck_5_ck').prop('checked', true);
-            $("#no_rating").removeClass("visible").addClass("invisible");
+            $("#no_rating").hide();
         }
+        form.addClass("was-validated");
+        return formIsValid;
     }
-    ;
-
-    $('#pop_claim').on('click', function (event) {
+    
+    form.on('submit', function (event) {
+        //preventin browser's default action also by returning false
         event.preventDefault();
         event.stopPropagation();
-        $('#ldp-uri').val("https://farewellutopia.com:8443/");
-        $('#tweet_url').val("https://twitter.com/wesbos/status/912736261630251008");
-        $('#claim_reviewed').val("That Facebook has decided to relicense React & Friends to MIT.");
-        $('#review_body').val("Wes Bos is a reliable developer and has no reason to lie.");
-        $('#rating-in-stars').val(5);
-    });
-
-    $('#update_tweet').on('click', function (event) {
-        var twitframe = document.getElementById("twitframe");
-        var tweetURI = $('#tweet_url').val();
-        twitframe.src = "https://twitframe.com/show?url=" + encodeURIComponent(tweetURI);
-    });
-
-    $('#submit_claim').on('click', function (event) {
-        // setup for this submit.
-        event.preventDefault();
-        event.stopPropagation();
+        submissionAttempted = true;
         $('#claimreview_text').val("");
         $('#claimreview_text').addClass('hide').removeClass('show');
         $("#cr-valid").removeClass("show").addClass("hide");
         $("#cr-invalid").removeClass("show").addClass("hide");
-        var tweetURI = $('#tweet_url').val();
+        var tweetUri = new TweetUri($('#tweet_url').val());
         var today = new Date();
         var today_iso = today.toISOString().slice(0, 10);
-        var rating_int = $('#rating-in-stars').val();
-        var rating_alt = "";
+        var rating = Number($('#rating-in-stars').val());
         var claim_reviewed = $('#claim_reviewed').val();
         var review_body = $('#review_body').val();
-        var form = document.getElementById("needs-validation");
-        if (rating_int == 0) {
-            $('#invalid').html("Please rate the truthfulness of the Tweet");
-            form.classList.add("was-validated");
+        
+        var formIsValid = checkForm();
+        
+        if (!formIsValid || (rating === 0)) {
+            $('#invalid').html("Please rate the truthfulness of the Tweet");            
         } else {
-            if (!form.checkValidity()) {
-                //event.preventDefault();
-                //event.stopPropagation();
-                form.classList.add("was-validated");
-            } else {
-                form.classList.add("was-validated");
-                switch (rating_int) {
-                    case '1':
-                        rating_alt = "Flat out lie";
-                        break;
-                    case '2':
-                        rating_alt = "Quicksand";
-                        break;
-                    case '3':
-                        rating_alt = "Somewhat true";
-                        break;
-                    case '4':
-                        rating_alt = "Mostly true";
-                        break;
-                    case '5':
-                        rating_alt = "Rock solid truth";
-                        break;
-                    default:
-                        rating_alt = "May or may not be true";
+            function ratingLabel(value) {
+                switch (value) {
+                    case 1: return "Flat out lie";
+                    case 2: return "Quicksand";
+                    case 3: return "Somewhat true";
+                    case 4: return "Mostly true";
+                    case 5: return "Rock solid truth";
+                    default: return "May or may not be true";
                 }
-
-                // if all is good: create claimreview and save.
-                function schema(suffix) {
-                    return $rdf.sym("http://schema.org/" + suffix);
-                }
-                function rdf(suffix) {
-                    return $rdf.sym("http://www.w3.org/1999/02/22-rdf-syntax-ns#" + suffix);
-                }
-                var graph = $rdf.graph();
-
-                var claimReview = $rdf.sym("http://review.local/");
-                graph.add(claimReview, rdf("type"), schema("ClaimReview"));
-                graph.add(claimReview, schema("claimedReviewed"), claim_reviewed);
-                graph.add(claimReview, schema("reviewBody"), review_body);
-                var itemReviewed = $rdf.sym(tweetURI);
-                graph.add(claimReview, schema("itemReviewed"), itemReviewed);
-                graph.add(claimReview, schema("datePublished"), $rdf.literal(today_iso, schema("Date")));
-                var reviewRating = $rdf.blankNode();
-                graph.add(reviewRating, rdf("type"), schema("Rating"));
-                graph.add(reviewRating, schema("ratingValue"), rating_int);
-                graph.add(reviewRating, schema("alternateName"), rating_alt);
-                graph.add(claimReview, schema("reviewRating"), reviewRating);
-                graph.add(claimReview, schema("url"), claimReview);
-                var data = new $rdf.Serializer(graph, $rdf.sym("https://twitter.com/")).setBase("http://review.local/").toN3(graph);
-                $('#claimreview_text').val(data);
-                $('#claimreview_text').addClass('show').removeClass('hide');
-                if (($('#claimreview_filename').length > 0) && ($('#claimreview_filename').val().length > 0)) {
-                    var slug = $('#claimreview_filename').val();
-                } else {
-                    var slug = default_timestamp();
-                }
-                var tweetUri = new TweetUri(tweetURI);
-                SolidUtils.getStorageRootContainer().then(function (root) {
-                    return SolidUtils.createPath(root.value + "public", "twee-fi/" + tweetUri.getUser() + "/" + tweetUri.getStatus()).then(
-                            function (defaultContainer) {
-                                return SolidAuthClient.fetch(defaultContainer, {
-                                    'method': 'POST',
-                                    'body': data,
-                                    'headers': {
-                                        'Content-Type': 'text/turtle',
-                                        'slug': slug
-                                    }
-                                }).then(function (meta) {
-                                    console.log("Success! Sent payload to designated LDP-URI!");
-                                    $("#cr-valid").removeClass("hide").addClass("show");
-                                    $("#cr-invalid").removeClass("show").addClass("hide");
-                                }).catch(function (err) {
-                                    // do something with the error
-                                    console.log(err);
-                                    $('#cr_error_msg').text(err);
-                                    $("#cr-invalid").addClass("show").removeClass("hide");
-                                    $("#cr-valid").removeClass("show").addClass("hide");
-                                });
-                            });
-                });
             }
+            var rating_alt = ratingLabel(rating);
+
+            // if all is good: create claimreview and save.
+
+            var graph = $rdf.graph();
+
+            var claimReview = $rdf.sym("http://review.local/");
+            graph.add(claimReview, vocab.rdf("type"), vocab.schema("ClaimReview"));
+            graph.add(claimReview, vocab.schema("claimedReviewed"), claim_reviewed);
+            graph.add(claimReview, vocab.schema("reviewBody"), review_body);
+            var itemReviewed = $rdf.sym(tweetUri.toString());
+            graph.add(claimReview, vocab.schema("itemReviewed"), itemReviewed);
+            graph.add(claimReview, vocab.schema("datePublished"), $rdf.literal(today_iso, vocab.schema("Date")));
+            var reviewRating = $rdf.blankNode();
+            graph.add(reviewRating, vocab.rdf("type"), vocab.schema("Rating"));
+            graph.add(reviewRating, vocab.schema("ratingValue"), rating);
+            graph.add(reviewRating, vocab.schema("alternateName"), rating_alt);
+            graph.add(claimReview, vocab.schema("reviewRating"), reviewRating);
+            graph.add(claimReview, vocab.schema("url"), claimReview);
+            var data = new $rdf.Serializer(graph, $rdf.sym("https://twitter.com/")).setBase("http://review.local/").toN3(graph);
+            $('#claimreview_text').val(data);
+            $('#claimreview_text').addClass('show').removeClass('hide');
+            if (($('#claimreview_filename').length > 0) && ($('#claimreview_filename').val().length > 0)) {
+                var slug = $('#claimreview_filename').val();
+            } else {
+                var slug = default_timestamp();
+            }
+            SolidUtils.getStorageRootContainer().then(function (root) {
+                return SolidUtils.createPath(root.value + "public", "twee-fi/" + tweetUri.getUser() + "/" + tweetUri.getStatus()).then(
+                        function (defaultContainer) {
+                            return SolidAuthClient.fetch(defaultContainer, {
+                                'method': 'POST',
+                                'body': data,
+                                'headers': {
+                                    'Content-Type': 'text/turtle',
+                                    'slug': slug
+                                }
+                            }).then(function (meta) {
+                                console.log("Success! Sent payload to designated LDP-URI!");
+                                $("#cr-valid").removeClass("hide").addClass("show");
+                                $("#cr-invalid").removeClass("show").addClass("hide");
+                            }).catch(function (err) {
+                                // do something with the error
+                                console.log(err);
+                                $('#cr_error_msg').text(err);
+                                $("#cr-invalid").addClass("show").removeClass("hide");
+                                $("#cr-valid").removeClass("show").addClass("hide");
+                            });
+                        });
+            });
+
         }
+        return false;
     });
 });
