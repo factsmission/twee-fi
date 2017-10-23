9e0799472142b7b97de5dc70cb0b97892044f3de
alemela <alessiom92@gmail.com>
2017-10-23 23:46:07 +0200

    improved view and propagated into menus

--- js/view.js
+++ js/view.js
@@ -7,13 +7,31 @@ $(function () {
         reviewObject.claim = review.out(SolidUtils.vocab.schema("claimReviewed")).value;
         reviewObject.review = review.out(SolidUtils.vocab.schema("reviewBody")).value;
         reviewObject.rating = review.out(SolidUtils.vocab.schema("reviewRating")).out(SolidUtils.vocab.schema("ratingValue")).value;
-        console.log(reviewObject);
+        //console.log(reviewObject);
 
-        $("#muTemp").load("/template/view.html", function(res, status, xhr) {
-            var template = document.getElementById('reviewTemplate').innerHTML;
-            var output = Mustache.render(template, reviewObject);
-            $("#tweets").append(output).fadeIn(2000);
+        reviewObject.stars = "";
+        for (var r = 0; r < reviewObject.rating; r++) {
+            reviewObject.stars += '<i class="fa fa-star"></i>';
+        }
+        for (r; r < 5; r++) {
+            reviewObject.stars += '<i class="fa fa-star-o"></i>';
+        }
+
+        $.ajax({
+            url: "https://publish.twitter.com/oembed?url=" + reviewObject.tweet,
+            dataType: 'jsonp',
+            success: function (tweet) {
+                reviewObject.tweetBlockquote = tweet.html;
+
+                $("#muTemp").load("/template/view.html", function(res, status, xhr) {
+                    var template = document.getElementById('reviewTemplates').innerHTML;
+                    var output = Mustache.render(template, reviewObject);
+                    $("#tweets").append(output).fadeIn(2000);
+                });
+            }
         });
+
+
     }
 
     SolidUtils.login().then(function () {
@@ -30,7 +48,7 @@ $(function () {
                             tweet.fetch().then(tweet => {
                                 tweet.out($rdf.sym("http://www.w3.org/ns/ldp#contains")).each(review => {
                                     review.fetch().then(review => {
-                                        console.log(twitterUser.value, tweet.value, review.value);
+                                        //console.log(twitterUser.value, tweet.value, review.value);
                                         drawReview(review);
                                     });
                                 });
