f408ef457086133034dcb5f2d747caf790814f48
alemela <alessiom92@gmail.com>
2017-10-23 18:57:39 +0200

    init view page with mustache templates

--- js/view.js
+++ js/view.js
@@ -1,6 +1,21 @@
 /* global SolidUtils, TweeFiUtils, $rdf, GraphNode */
 
 $(function () {
+    function drawReview (review) {
+        var reviewObject = {};
+        reviewObject.tweet = review.out(SolidUtils.vocab.schema("itemReviewed")).value;
+        reviewObject.claim = review.out(SolidUtils.vocab.schema("claimReviewed")).value;
+        reviewObject.review = review.out(SolidUtils.vocab.schema("reviewBody")).value;
+        reviewObject.rating = review.out(SolidUtils.vocab.schema("reviewRating")).out(SolidUtils.vocab.schema("ratingValue")).value;
+        console.log(reviewObject);
+
+        $("#muTemp").load("/template/view.html", function(res, status, xhr) {
+            var template = document.getElementById('reviewTemplate').innerHTML;
+            var output = Mustache.render(template, reviewObject);
+            $("#tweets").append(output).fadeIn(2000);
+        });
+    }
+
     SolidUtils.login().then(function () {
         TweeFiUtils.updateLoginInfo();
         return SolidUtils.getStorageRootContainer().then(function (root) {
@@ -10,15 +25,13 @@ $(function () {
             {
                 folder.out($rdf.sym("http://www.w3.org/ns/ldp#contains")).each(twitterUser =>
                 {
-                    console.log(twitterUser.value);
                     twitterUser.fetch().then(twitterUser => {
                         twitterUser.out($rdf.sym("http://www.w3.org/ns/ldp#contains")).each(tweet => {
-                            console.log(tweet.value);
                             tweet.fetch().then(tweet => {
                                 tweet.out($rdf.sym("http://www.w3.org/ns/ldp#contains")).each(review => {
-                                    console.log(review.value);
                                     review.fetch().then(review => {
-                                        console.log(review.out(SolidUtils.vocab.schema("claimReviewed")).value);
+                                        console.log(twitterUser.value, tweet.value, review.value);
+                                        drawReview(review);
                                     });
                                 });
                             });
