dc177b4e5bc697cae1b7b97629daff53da7ff79b
Noam Bachmann <noam@factsmission.com>
2018-01-21 14:46:18 +0100

    changed appearance of view.html and if in any review some data is missing, the delete buttoin still works everywhere.

--- js/view.js
+++ js/view.js
@@ -3,12 +3,32 @@
 $(function () {
     function drawReview (review) {
         var reviewObject = {};
-        reviewObject.tweet = review.out(SolidUtils.vocab.schema("itemReviewed")).value;
-        reviewObject.claim = review.out(SolidUtils.vocab.schema("claimReviewed")).value;
-        reviewObject.review = review.out(SolidUtils.vocab.schema("reviewBody")).value;
-        reviewObject.rating = review.out(SolidUtils.vocab.schema("reviewRating")).out(SolidUtils.vocab.schema("ratingValue")).value;
+
+        try {
+            reviewObject.review = review.out(SolidUtils.vocab.schema("reviewBody")).value;
+        } catch (error) {
+            console.error("Coluld not load review of " + review.value + " : " + error);
+        }
+
+        try {
+            reviewObject.claim = review.out(SolidUtils.vocab.schema("claimReviewed")).value;
+        } catch (error) {
+            console.error("Coluld not load claim of " + review.value + " : " + error);
+        }
+
+        try {
+            reviewObject.tweet = review.out(SolidUtils.vocab.schema("itemReviewed")).value;
+        } catch (error) {
+            console.error("Coluld not load reviewed item of " + review.value + " : " + error);
+        }
+
+        try {
+            reviewObject.rating = review.out(SolidUtils.vocab.schema("reviewRating")).out(SolidUtils.vocab.schema("ratingValue")).value;
+        } catch (error) {
+            console.error("Coluld not load rating of " + review.value + " : " + error);
+        }
+
         reviewObject.uri = review.value;
-        //console.log(reviewObject);
 
         reviewObject.stars = "";
         for (var r = 0; r < reviewObject.rating; r++) {
@@ -21,34 +41,52 @@ $(function () {
         return $.ajax({
             url: "https://publish.twitter.com/oembed?url=" + reviewObject.tweet,
             dataType: 'jsonp'
-        }).then(
-            function (tweet) {
+        }).catch((e) => {
+            console.log("ERORR EST: " + e);
+        }).then((tweet) => {
+            if (typeof tweet === "undefined") {
+                console.log("ERROR EST: tweet is undefined");
+            } else {
                 reviewObject.tweetBlockquote = tweet.html;
-
-                return $.ajax("./template/view.html").then(function(template) {
+                return $.ajax("./template/view.html").catch(() => {
+                    console.log("ERORREM EST: " + e);
+                }).then(function (template) {
                     //var template = document.getElementById('reviewTemplates').innerHTML;
-                    var output = Mustache.render(template, reviewObject);
-                    $("#tweets").append(output).fadeIn(2000);
+                    if (Object.keys(reviewObject).length > 0) {
+                        var output = Mustache.render(template, reviewObject);
+                        $("#tweets").append(output).fadeIn(2000);
+                    }
                 });
-            });
+            }
+        });
 
 
     }
     
     TweeFiUtils.updateLoginInfo();
     var reviewCount = 0;
-    SolidUtils.getStorageRootContainer().then(function (root) {
+    SolidUtils.getStorageRootContainer().catch(function (error) {
+        console.error("Couldn't get storage root: " + error);
+    }).then(function (root) {
         var rootURI = root.value + "public/twee-fi/";
         var tweefiRoot = $rdf.sym(rootURI);
-        return GraphNode(tweefiRoot).fetch().then(folder =>
+        return GraphNode(tweefiRoot).fetch().catch(function (error) {
+            console.error("Couldn't fetch " + tweefiRoot +": " + error);
+        }).then(folder =>
         {
             return folder.out($rdf.sym("http://www.w3.org/ns/ldp#contains")).each(twitterUser =>
             {
-                return twitterUser.fetch().then(twitterUser => {
+                return twitterUser.fetch().catch(function (error) {
+                    console.error("Couldn't fetch /" + twitterUser + ": " + error);
+                }).then(twitterUser => {
                     return twitterUser.out($rdf.sym("http://www.w3.org/ns/ldp#contains")).each(tweet => {
-                        return tweet.fetch().then(tweet => {
+                        return tweet.fetch().catch(function (error) {
+                            console.error("Couldn't fetch /" + tweet + ": " + error);
+                        }).then(tweet => {
                             return tweet.out($rdf.sym("http://www.w3.org/ns/ldp#contains")).each(review => {
-                                return review.fetch().then(review => {
+                                return review.fetch().catch(function (error) {
+                                    console.error("Couldn't fetch /" + review + ": " + error);
+                                }).then(review => {
                                     //console.log(twitterUser.value, tweet.value, review.value);
                                     reviewCount++;
                                     return drawReview(review);
@@ -59,20 +97,19 @@ $(function () {
                 });
             });
         });
-    }).catch(function (error) {
-        console.log("Couldn't get storage root: " + error);
     }).then(() => {
         if (reviewCount === 0) {
             $("#tweets").append("No reviews! Go review some tweets.");
         }
         console.log("adding event listner to "+$(".delete").length+" elements");
+        
         $(".delete").click(e => {
             var review = $(e.target).closest(".review");
             var origBG = review.css("background-color");
-            review.css( "background-color", "red" )
+            review.addClass("bg-danger text-white");
             setTimeout(() => { 
-                    if (confirm("Delete "+ e.target.value+"?")) {
-                        return SolidAuthClient.fetch(e.target.value, {
+                    if (confirm("Delete "+ review.find(".permalink").attr("href") +" ?")) {
+                        return SolidUtils.fetch(review.find(".permalink").attr("href"), {
                                 method: 'delete'
                             }).then(response => {
                                 console.log(response);
@@ -81,10 +118,9 @@ $(function () {
                                 } else {
                                     response.text().then(msg => alert(msg));
                                 }
-                                
                             });
                     } else {
-                        review.css( "background-color", origBG)
+                        review.removeClass("bg-danger text-white");
                     }
                 }, 1);
         });
